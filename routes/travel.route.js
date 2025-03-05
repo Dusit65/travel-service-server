c74db2f809d54f532878c4c9256263b40b7b9375
Dusit Pakdeekunakorn <S6552410011@sau.ac.th>
2025-03-05 16:29:30 +0700

    v4.2.2 Image Path Fixed

--- routes/travel.route.js
+++ routes/travel.route.js
@@ -10,6 +10,7 @@ const router = express.Router();
 //GET = ค้นหา ตรวจสอบ ดึง ดู, POST = เพิ่ม, PUT = แก้ไข, DELETE = ลบ
 router.post("/", travelCtrl.uploadTravel, travelCtrl.createTravel);
 router.get("/:travellerId", travelCtrl.getAllTravel);
+router.get("/one/:travelId", travelCtrl.getTravel); //get selected travel
 router.put("/:travelId", travelCtrl.uploadTravel, travelCtrl.editTravel);
 router.delete("/:travelId", travelCtrl.deleteTravel);
 
